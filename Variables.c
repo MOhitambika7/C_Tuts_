9a0cbd843b9317d5169545322d19bd4eebbba60a
MOhitambika7 <mohitambikanand7@gmail.com>
2025-01-11 09:21:40 +0545

    This is the first practice code for variables in c programming langugae.

---
+++ Variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int Number = 5;
+    char Star = '*';
+
+    int age = 12;
+    float price = 12.1212122f;
+
+    printf("The given number is:- %d\n ", Number);
+    printf("The given character is:- %c\n", Star);
+    printf("The given age is:- %d\n", age);
+    printf("The given floating price value is:- %f", price);
+
+    return 0;
+}
