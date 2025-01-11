eaae0d6a1dd358d668cbe2ef291c40e644be15ad
MOhitambika7 <mohitambikanand7@gmail.com>
2025-01-11 11:25:05 +0545

    This is the code for taking user input and printing the value.

---
+++ Taking_Input.c
@@ -0,0 +1,15 @@
+/*Here in this code we are going to learn taking input from the user and print that input values.*/
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter the number a:- ");
+    scanf("%d", &a);
+    printf("The number a is:- %d\n", a);
+
+    printf("Enter the number b:- ");
+    scanf("%d", &b);
+    printf("The number b is:- %d\n", b);
+
+    return 0;
+}
